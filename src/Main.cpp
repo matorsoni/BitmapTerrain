9975107fa22c0e85ee05b35af4ef6a20d5580d9a
Mateus Orsoni <matorsoni45@gmail.com>
2022-09-07 12:16:02 -0300

    Add GLFW as submodule and create a blank window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
 
+// Macro to avoid GLFW including OpenGL header on it's own.
+// Ref: https://www.glfw.org/docs/3.3/quick.html#quick_include
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+
+using std::cout;
+using std::endl;
+
+// Create GLFW error callback function.
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error: %s\n", description);
+}
+
 int main()
 {
-    std::cout << "Vamo que vamo!" << std::endl;
+    // Initialize GLFW with error callback.
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        cout << "Failed to initialize GLFW." << endl;
+        return -1;
+    }
+
+    // Set minimum OpenGL core profile version expected by the context.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    // Create window.
+    const int window_width = 1280;
+    const int window_height = 720;
+    GLFWwindow* window = glfwCreateWindow(window_width,
+                                          window_height,
+                                          "BitmapTerrain",
+                                          NULL,
+                                          NULL);
+    if (!window) {
+        cout << "Failed to create window." << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Main loop.
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    // GLFW cleanup.
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
