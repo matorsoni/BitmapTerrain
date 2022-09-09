be9fc14b762f4ef0556f17a847813cb780b40fb4
Mateus Orsoni <matorsoni45@gmail.com>
2022-09-09 13:38:06 -0300

    Add some color to the terrain via fragment shader

--- src/shader/Frag.glsl
+++ src/shader/Frag.glsl
@@ -1,8 +1,16 @@
 #version 400 core
 
+in float y;
 out vec4 frag_color;
+
+const vec3 color0 = vec3(0.8, 0.8, 0.0);
+const vec3 color1 = vec3(1.0, 0.0, 0.0);
+vec3 color_lerp(float t)
+{
+    return (1 - t) * color0 + t * color1;
+}
 
 void main()
 {
-    frag_color = vec4(1.0, 0.0, 0.0, 1.0);
+    frag_color = vec4(color_lerp(y), 1.0);
 }
--- src/shader/Vert.glsl
+++ src/shader/Vert.glsl
@@ -6,7 +6,10 @@ uniform mat4 Model;
 uniform mat4 View;
 uniform mat4 Projection;
 
+out float y;
+
 void main()
 {
     gl_Position = Projection * View * Model * vec4(pos, 1.0);
+    y = clamp(pos.y, 0.0, 1.0);
 }
